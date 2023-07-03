4a8ed68703135a271f64f028a2cf8391b941ce2d
mertsevinc-dev <mert.sevinc.dev@gmail.com>
2023-07-03 14:50:46 +0900

    added contact Models

---
+++ models/contactModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxlength: [50, "Name cannot be more than 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      maxlength: [50, "Email cannot be more than 50 characters"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone is required"],
+      trim: true,
+      maxlength: [20, "Phone cannot be more than 20 characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Contact", contactSchema);
